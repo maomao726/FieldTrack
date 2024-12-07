a4a4d62d5550f61809e987f6247e051b733bf3f6
maomao726 <l0980071172@gmail.com>
2024-12-07 18:00:14 +0800

    sync with trackers

--- eval_my.sh
+++ eval_my.sh
@@ -1,25 +1,37 @@
-# Description: Evaluate the tracking results of the football dataset using the FieldTrack algorithm
+# Description: 用于运行 OC_SORT 的评估脚本
 
 ############################################
-paths=( 
-    #    "/media/hcchen/data/data/football_data/videoData/motions/slowest" 
-    #    "/media/hcchen/data/data/football_data/videoData/motions/slow" 
-    #    "/media/hcchen/data/data/football_data/videoData/motions/mediumslow"
-    #    "/media/hcchen/data/data/football_data/videoData/motions/mediumfast"
-    #    "/media/hcchen/data/data/football_data/videoData/motions/fast"
-    #    "/media/hcchen/data/data/football_data/videoData/motions/fastest"
-         "/media/hcchen/data/data/football_data/videoData/cut2" 
-        # "/media/hcchen/data/data/football_data/videoData/nocut0" 
-        # "/media/hcchen/data/data/football_data/videoData/nocut1"
-        # "/media/hcchen/data/data/football_data/videoData/nocut2"
-        )
+paths=(
+    # "/media/hcchen/backup/DataCollection/CARLA_0.9.15/carla_combined/cut"
+    # "/media/hcchen/backup/DataCollection/CARLA_0.9.15/carla_combined/slowest"
+    # "/media/hcchen/backup/DataCollection/CARLA_0.9.15/carla_combined/slow"
+    # "/media/hcchen/backup/DataCollection/CARLA_0.9.15/carla_combined/fast"
+    # "/media/hcchen/backup/DataCollection/CARLA_0.9.15/carla_combined/fastest"
+     "/media/hcchen/307cc387-4594-4a7a-b426-1994a987f8c4/football_data/football_combined/cut"
+    # "/media/hcchen/307cc387-4594-4a7a-b426-1994a987f8c4/football_data/football_combined/slowest"
+    # "/media/hcchen/307cc387-4594-4a7a-b426-1994a987f8c4/football_data/football_combined/slow"
+    # "/media/hcchen/307cc387-4594-4a7a-b426-1994a987f8c4/football_data/football_combined/fast"
+    # "/media/hcchen/307cc387-4594-4a7a-b426-1994a987f8c4/football_data/football_combined/fastest"
+      )
 
-raw_path="/media/hcchen/data/data/football_data/dataset_.98"
-#raw_path="/media/hcchen/data/data/football_data/maincam_momentum"
-#raw_path="/media/hcchen/backup/DataCollection/CARLA_0.9.15/output_2"
+#raw_path="/media/hcchen/backup/DataCollection/CARLA_0.9.15/carla_combined"
+raw_path="/media/hcchen/307cc387-4594-4a7a-b426-1994a987f8c4/football_data/football_combined"
 
-# Pretrained method
-method="MOT17" # MOT17, MOT20, Dance
+# 选择排序方法
+method="MOT20" # MOT17, MOT20(football), Dance, Carla
+
+# Field model path
+#field_model_path="/media/hcchen/data/Trackers/shared_weights/field/field_football.pth"
+#field_model_path="/media/hcchen/data/football_extension/carla/training/combined_0/best.pth"
+field_model_path="/mnt/12cf47fc-dac6-4abb-a06f-014c3d2e7d30/football_extension/football_combined/training_0/best.pth"
+
+out_path="results_carla/eval_hist_my_${method}.csv"
+
+## 使用預先定義的bounding box(Carla only)
+detection_prepared=False
+
+## output_dir
+output_dir="football" # football, carla
 ############################################
 
 
@@ -39,29 +51,36 @@ elif [ $method == "MOT20" ]; then
 elif [ $method == "Dance" ]; then
     exp_file="exps/example/mot/yolox_dancetrack_test.py"
     ckpt="pretrained/ocsort_dance_model.pth.tar"
+elif [ $method == "Carla" ]; then
+    #ckpt="/media/hcchen/data/YOLOX/YOLOX/YOLOX_outputs/yolox_custom/best_ckpt.pth"
+    ckpt="pretrained/ocsort_dance_model.pth.tar"
+    #exp_file="/media/hcchen/data/YOLOX/YOLOX/exps/example/custom/yolox_custom.py"
+    exp_file="exps/example/mot/yolox_dancetrack_test.py"
 else
     echo "Invalid method"
     exit 1
 fi
 sed -i "s|self.exp_file = .*|self.exp_file = \"$exp_file\"|" FieldTrack/config/eval_my.py
 sed -i "s|self.ckpt = .*|self.ckpt = \"$ckpt\"|" FieldTrack/config/eval_my.py
+sed -i "s|self.field_pretrained = .*|self.field_pretrained = \"$field_model_path\"|" FieldTrack/config/eval_my.py
+sed -i "s|self.output_dir = .*|self.output_dir = \"$output_dir\"|" FieldTrack/config/eval_my.py
+sed -i "s|self.detection_prepared = .*|self.detection_prepared = $detection_prepared|" FieldTrack/config/eval_my.py
 
 
-out_path="results/eval_hist_my_${method}.csv"
 
 # 循环替换路径并运行 Python 程序
 for path in "${paths[@]}"; do
-    pred_path="football/$(basename $path)"
+    pred_path="$output_dir/$(basename $path)"
 
     echo "Running script with path: $path"
 
-    cd FieldTrack
-    sed -i "s|self.path = .*|self.path = \"$path\"|" config/eval_my.py
-    # 运行修改后的 Python 程序
-    python eval_football_track.py
+    # cd FieldTrack
+    # sed -i "s|self.path = .*|self.path = \"$path\"|" config/eval_my.py
+    # # 运行修改后的 Python 程序
+    # python eval_football_track.py
 
-    cd ..
-    python evaluate.py $raw_path $path "FieldTrack/${pred_path}" --output $out_path
+    # cd ..
+    python evaluate.py $raw_path $path "FieldTrack/${pred_path}"
 done
 
 # 恢复原始的 main.py 文件
--- evaluate.py
+++ evaluate.py
@@ -12,20 +12,46 @@ def eval(args):
     mh = mm.metrics.create()
     summaries : pd.DataFrame = None
 
-    pred_files = [p for p in os.listdir(args.predict_path) if p.endswith('.txt')]
+    # DA_acc_list = []
+    # DA_on_new_list = []
+    # DA_on_appear_list = []
+
+    pred_files = sorted([p for p in os.listdir(args.predict_path) if p.endswith('.txt')])
     for pred_file in tqdm(pred_files):
-        print(pred_file)
+        #print(f"Processing {pred_file}")
+        #
+        # id_map = {}
+        # appeared_id = []
+        # DA_acc_list.append(0)
+        # DA_on_new_list.append(0)
+        # DA_on_appear_list.append(0)
+
         pred_total = np.loadtxt(os.path.join(args.predict_path, pred_file), dtype=np.float32, delimiter=',')
-        
+        if len(pred_total.shape) == 1:
+            pred_total = pred_total.reshape(-1, 4)
         gt_parser.set_tracking_video(pred_file[:-4])
   
-        
-        for frameid in range(int(np.max(pred_total[:,0]) + 1)):
+        for frameid in range(len(gt_parser.frame_table)):
             pred = pred_total[pred_total[:, 0] == frameid]
-            
             gt_id, pred_id, c = gt_parser.compare_with_gt(frameid, pred[:, [2, 3, 1]])
             acc.update(gt_id, pred_id, c, frameid=frameid)
         
+            # events_t = acc.events.reset_index()
+            # events = events_t[events_t['FrameId'] == frameid]
+
+            # for _, m in events[events['Type'] == 'MATCH'].iterrows():
+            #     if m['HId'] not in id_map:
+            #         id_map[m['HId']] = m['OId']
+            #     else:
+            #         if id_map[m['HId']] != m['OId']:
+            #             if m['OId'] not in appeared_id:
+            #                 DA_on_new_list[-1] += 1
+            #             if m['OId'] not in id_map.values():
+            #                 DA_on_appear_list[-1] += 1
+            #             DA_acc_list[-1] += 1
+            #     if m['OId'] not in appeared_id:
+            #         appeared_id.append(m['OId'])
+            
         summary = mh.compute(acc, metrics=mm.metrics.motchallenge_metrics, name=pred_file[:-4])
         #print(mm.io.render_summary(summary, formatters=mh.formatters, namemap=mm.io.motchallenge_metric_names))
 
@@ -39,8 +65,12 @@ def eval(args):
     
     print("------Done------")
     summaries_str = mm.io.render_summary(summaries, formatters=mh.formatters, namemap=mm.io.motchallenge_metric_names)
-    print(summaries_str)
+    #print(summaries_str)
 
+    # print("--------DA--------")
+    # print(f"DA: {np.sum(DA_acc_list)}, min: {np.min(DA_acc_list)}, max: {np.max(DA_acc_list)}, mean: {np.mean(DA_acc_list)}")
+    # print(f"DA_on_new: {np.sum(DA_on_new_list)}, min: {np.min(DA_on_new_list)}, max: {np.max(DA_on_new_list)}, mean: {np.mean(DA_on_new_list)}")
+    # print(f"DA_on_appear: {np.sum(DA_on_appear_list)}, min: {np.min(DA_on_appear_list)}, max: {np.max(DA_on_appear_list)}, mean: {np.mean(DA_on_appear_list)}")
     summaries_stats = {
         'video_path' : args.video_path.split('videoData/')[-1],
         'mota' : 1 - (summaries['num_false_positives'].sum() + summaries['num_misses'].sum() + summaries['num_switches'].sum()) / summaries['num_objects'].sum(),
@@ -50,12 +80,20 @@ def eval(args):
         'idsw' : summaries['num_switches'].sum(),
         'gt' : summaries['num_objects'].sum(),
         'idp' : summaries['idtp'].sum() / (summaries['idtp'].sum() + summaries['idfp'].sum()),
-        'idr' : summaries['idtp'].sum() / (summaries['idtp'].sum() + summaries['idfn'].sum())
+        'idr' : summaries['idtp'].sum() / (summaries['idtp'].sum() + summaries['idfn'].sum()),
+        'idt' : summaries['num_transfer'].sum(),
+        'ida' : summaries['num_ascend'].sum(),
+        'idm' : summaries['num_migrate'].sum()
     }
 
+    for k, v in summaries_stats.items():
+        if isinstance(v, np.float64):
+            summaries_stats[k] = round(v, 4)
+        #summaries_stats[k] = round(v, 4)
     print(f"Summary Stats for all videos: ")
     print("\n".join([f"{k}: \t {v}" for k, v in summaries_stats.items()]))
 
+   
     # save to csv
     if args.output is not None:
         if os.path.exists(args.output):
